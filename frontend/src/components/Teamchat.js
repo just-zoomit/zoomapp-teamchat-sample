d6cbd7e710261206ba23f131c4e0f8e1221a2f74
just-zoomit <donte.zoomie@gmail.com>
2024-04-23 11:48:16 -0400

    first commit, added Zoom App Chat APIs and component

---
+++ frontend/src/components/Teamchat.js
@@ -0,0 +1,13 @@
+import "./Teamchat.css"
+function Teamchat() {
+  return (
+    <div className="Teamchat-sample">
+        <h1>Teamchat</h1>
+        <p>Teamchat is a feature that allows you to chat with your team members.</p>
+        <p>It is a great way to communicate and collaborate with your team.</p>
+        <p>Teamchat is available in the Zoom Apps sidebar.</p>
+    </div>
+  );
+}
+
+export default Teamchat;
